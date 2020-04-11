46993e2903cfeb1f465ad95b33f41acb35abc9e3
DulioCagg <duliop.caggiano@hotmail.com>
2020-04-11 13:43:07 -0500

    db conection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-rigid-99332',
-    user: 'dulio',
-    password: ' ',
-    database: 'final_project'
+    host: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
